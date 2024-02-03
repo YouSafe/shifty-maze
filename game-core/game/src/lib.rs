f0cfc52882e340d2d4160bc6b047d2477368d0ec
nemegyeb <nemegyeb@gmail.com>
2024-02-03 18:25:42 +0100

    Implement `Board::new`

--- game-core/game/src/lib.rs
+++ game-core/game/src/lib.rs
@@ -2,3 +2,13 @@ pub mod board;
 pub mod game;
 pub mod player;
 pub mod tile;
+
+#[cfg(test)]
+mod tests {
+    use crate::board::Board;
+
+    #[test]
+    fn it_works() {
+        Board::new(0, 7);
+    }
+}
