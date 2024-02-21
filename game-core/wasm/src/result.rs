3ef388ba1a1e5af36581ab8ecf9d75db429bf6d7
nemegyeb <80166690+nemegyeb@users.noreply.github.com>
2024-02-21 17:41:17 +0000

    Fix serialization of `Result` (#31)
    This PR fixes `Result` such that it is serialized into the correct
    Typescript type.

--- game-core/wasm/src/result.rs
+++ game-core/wasm/src/result.rs
@@ -3,16 +3,17 @@ use tsify::Tsify;
 
 #[derive(Tsify, Serialize)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct Result<T, E> {
-    ok: bool,
-    #[tsify(type = "T | E")]
-    value: std::result::Result<T, E>,
+#[serde(tag = "type", content = "value")]
+pub enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
 
-impl<T: Serialize, E: Serialize> From<std::result::Result<T, E>> for Result<T, E> {
+impl<T, E> From<std::result::Result<T, E>> for Result<T, E> {
     fn from(value: std::result::Result<T, E>) -> Self {
-        let ok = value.is_ok();
-
-        Self { ok, value }
+        match value {
+            Ok(ok) => Result::Ok(ok),
+            Err(err) => Result::Err(err),
+        }
     }
 }
